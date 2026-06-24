b777ab924c61dd7a73b707133adb955e1703b96c
DeweQ <thedeweq@yandex.ru>
2026-06-24 23:57:31 +0300

    Add boundary checks

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -6,6 +6,7 @@ module Chess
     def initialize(color)
       super
       @symbol = "♟".colorize(color) # color == :white ? "♙" : "♟"
+      @direction = color == :white ? 1 : -1
     end
 
     def check_move(move, board)
@@ -13,17 +14,27 @@ module Chess
     end
 
     def possible_moves(position, board)
+      result = advance_moves(position, board)
+      result + attack_moves(position, board)
+    end
+
+    def attack_moves(position, board)
       result = []
-      direction = color == :white ? 1 : -1
+      [-1, 1].each do |i|
+        next unless board.in_bounds(newpos = [position[0] + direction, position[1] + i])
+
+        result << newpos if board.at(newpos)&.color != color
+      end
+      result
+    end
+
+    def advance_moves(position, board)
+      result = []
       (1..2).each do |i|
-        newpos = [position[0] + (i * direction), position[1]]
+        next unless board.in_bounds(newpos = [position[0] + (i * @direction), position[1]])
         break unless board.at(newpos).nil?
 
         result << newpos
-      end
-      [-1, 1].each do |i|
-        newpos = [position[0] + direction, position[1] + i]
-        result << newpos if board.at(newpos)&.color != color
       end
       result
     end
